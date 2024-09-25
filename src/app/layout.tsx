cc92750fd33e225a0abe2c35e208f20f39b86c55
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-25 14:22:16 +0500

    Refactor RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-import localFont from "next/font/local";
 import { repositoryName } from "@/prismicio";
 import { PrismicPreview } from "@prismicio/next";
+import localFont from "next/font/local";
 
+import Header from "@/components/Header";
 import "./globals.css";
 
 const alpino = localFont({
-  src: "/assets/fonts/Alpino-Variable.woff2",
+  src: "../../public/assets/fonts/Alpino-Variable.woff2",
   display: "swap",
   variable: "--font-alpino",
 });
@@ -16,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${alpino.className}`}>{children}</body>
+    <html lang="en" className={alpino.variable}>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header />
+        <main>{children}</main>
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
