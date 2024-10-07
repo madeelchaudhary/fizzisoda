d4abbf89944485a23713a53f5fe85239909b9250
madeelchaudhry <madeelchaudhry@gmail.com>
2024-10-07 16:40:05 +0500

    Add Bubbles to drei View

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -12,6 +12,7 @@ import Button from "@/components/ui/Button";
 import Container from "@/components/ui/Container";
 import TextSplitter from "@/components/ui/TextSplitter";
 import Scene from "./Scene";
+import Bubbles from "./Bubbles";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
@@ -80,6 +81,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     >
       <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
         <Scene />
+        <Bubbles count={300} speed={2} repeat />
       </View>
       <div className="grid">
         <div className="grid h-screen place-items-center">
